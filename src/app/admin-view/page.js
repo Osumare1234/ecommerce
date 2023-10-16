eab6b3a93f45e7d79246a649e09d40ccda4c5ccb
OSUMARE EMMANUEL <emmanueulosumare@outlook.com git config --global user.name osumare4199git config --global user.name osumare4199git config --global user.email emmanueulosumare@outlook.com>
2023-10-16 01:10:16 +0100

    first commit

---
+++ src/app/admin-view/page.js
@@ -0,0 +1,13 @@
+
+
+import CommonListing from "@/components/CommonListing";
+import { getAllAdminProducts } from "@/services/product";
+
+
+
+export default async function AdminAllProducts() {
+
+  const allAdminProducts = await getAllAdminProducts()
+
+  return <CommonListing data={allAdminProducts && allAdminProducts.data}/>
+}
---
+++ src/components/Loader/componentlevel/index.js
@@ -0,0 +1,10 @@
+"use client"
+
+export default function ComponentLevelLoader({ text, color, loading, size }) {
+  return (
+    <span className="flex gap-1 items-center">
+      {text}
+     
+    </span>
+  );
+}
